21107881a918bbf4fdb8ecda0ce5459aeddd1a40
Alexey Ershkov <a.ershkov@corp.mail.ru>
2021-12-02 14:58:49 +0300

    settings update

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     'prettier/prettier': 'error',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
     'import/extensions': [
       'error',
       'ignorePackages',
